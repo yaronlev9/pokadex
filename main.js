4fabc71fb2e0f9010e48bb9cb37cc16345a831ff
yaronlev9 <62477626+yaronlev9@users.noreply.github.com>
2020-08-04 21:53:33 +0300

    finished pokadex

--- main.js
+++ main.js
@@ -1,5 +1,104 @@
-const searchPokemon = async (pokemonId = 3) => {
-  const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  console.log(data)
+const pokemonDiv = document.getElementById("results");
+const searchButton = document.getElementById("searchButon");
+const searchInput = document.getElementById("search");
+
+
+async function searchPokemon(){
+  let pokemonId = document.getElementById("search").value;
+  try{
+    const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    document.getElementById("search").value ="";
+    makeDiv(data.name, data.height, data.weight, data.sprites.front_default, data.sprites.back_default, data.types);
+  } catch(error) {
+    document.getElementById("search").value = "";
+    pokemonDiv.innerHTML="pokemon not found";
+    return;
+  }
 };
-searchPokemon();
+
+async function addPokemonNames(){
+  const options = {
+    method: "GET"
+  }
+  document.getElementById(this.id).removeEventListener("click", addPokemonNames);
+  document.getElementById(this.id).addEventListener("click", removePokemonNames);
+  let el = document.getElementById(this.id);
+  let lst = document.createElement("ol");
+  const data = await fetch(`http://pokeapi.co/api/v2/type/${this.id}`).then(response => response.json());
+  for (let name of data.pokemon){
+    let pokemon = document.createElement("li");
+    pokemon.setAttribute("id", name.pokemon.name);
+    pokemon.innerText = name.pokemon.name;
+    pokemon.addEventListener("click", getPokemonByName);
+    lst.appendChild(pokemon);
+  }
+  el.appendChild(lst); 
+}
+// axios.get(`http://pokeapi.co/api/v2/type/${this.id}`);
+function removePokemonNames(){
+  document.getElementById(this.id).children[0].remove();
+  document.getElementById(this.id).removeEventListener("click", removePokemonNames);
+  document.getElementById(this.id).addEventListener("click", addPokemonNames);
+}
+
+function getPokemonByName(){
+  document.getElementById("search").value = this.id;
+  searchPokemon();
+}
+
+function makeDiv (name, height, weight, picture, back_pic, types){
+  let typeArr = [];
+  for (let type of types){
+    typeArr.push(type.type.name);
+  }
+  let typesList = typeArr.join("</li><li> ");
+  let htmlText;
+  if (back_pic !== null){
+    if (picture !==null){
+      htmlText = `
+    <div class="pokemonContainer">
+      <div>Name: ${name}</div>
+      <div>height: ${height}</div>
+      <div>weight: ${weight}</div>
+      <div>picture: <br> <img src="${picture}" onmouseover="this.src='${back_pic}';" onmouseout="this.src='${picture}';"/></div>
+      <div id="types"><ul>types:<li id>${typesList}</li></ul></div>
+      </div>`;
+    } else {
+      htmlText = `
+    <div class="pokemonContainer">
+      <div>Name: ${name}</div>
+      <div>height: ${height}</div>
+      <div>weight: ${weight}</div>
+      <div>picture: <br> <img src="${back_pic}"></div>
+      <div id="types"><ul>types:<li id>${typesList}</li></ul></div>
+      </div>`;
+    }
+  } else {
+    if (picture !==null){
+      htmlText = `
+      <div class="pokemonContainer">
+        <div>Name: ${name}</div>
+        <div>height: ${height}</div>
+        <div>weight: ${weight}</div>
+        <div>picture: <br> <img src="${picture}"></div>
+        <div id="types"><ul>types:<li id>${typesList}</li></ul></div>
+        </div>`;
+    } else {
+      htmlText = `
+      <div class="pokemonContainer">
+        <div>Name: ${name}</div>
+        <div>height: ${height}</div>
+        <div>weight: ${weight}</div>
+        <div id="types"><ul>types:<li id>${typesList}</li></ul></div>
+        </div>`;
+    }
+  }
+  pokemonDiv.innerHTML = htmlText;
+  let lst = document.getElementById("types").children[0].children;
+  for (let i = 0; i < lst.length; i++){
+    lst[i].setAttribute("id", typeArr[i]);
+    lst[i].addEventListener("click", addPokemonNames);
+  }
+
+}
+searchButton.addEventListener("click", searchPokemon);
